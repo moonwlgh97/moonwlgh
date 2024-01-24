90d5470701fcb64bc58a75d00d4f5ecfad30a520
moonwlgh <moonwlgh@gmail.com>
2024-01-24 17:46:10 +0900

    0124 react

--- react/simplediary/src/App.js
+++ react/simplediary/src/App.js
@@ -1,11 +1,19 @@
-import { useState ,useRef, useEffect} from 'react';
-import './App.css';
-import DiaryEditor from './DiaryEditor';
-import DiaryList from './DiaryList';
+import {
+    useState,
+    useRef,
+    useEffect,
+    useMemo,
+    useCallback,
+    useReducer,
+} from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import OptimizeTest from "./OptimizeTest";
+
 // import Lifecycle from './Lifecycle';
 
 //https://jsonplaceholder.typicode.com/comments
-
 
 // const dummyList = [
 //   {
@@ -32,67 +40,99 @@ import DiaryList from './DiaryList';
 
 // ]
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "INIT": {
+            return action.data;
+        }
+        case "CREAT": {
+            const created_date = new Date().getTime();
+            const newItem = {
+                ...action.data,
+                created_date,
+            };
+            return [newItem, ...state];
+        }
+        case "REMOVE": {
+            return state.filter((it) => it.id !== action.targetId);
+        }
+        case "EDIT": {
+            return state.map((it) =>
+                it.id === action.targetId
+                    ? { ...it, content: action.newContent }
+                    : it,
+            );
+        }
+        default:
+            return state;
+    }
+};
 
 function App() {
-  const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
+    const [data, dispatch] = useReducer(reducer, []);
 
-  const dataId = useRef(0);
+    const dataId = useRef(0);
 
-  const getData =async()=>{
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res)=>res.json());
-    
+    const getData = async () => {
+        const res = await fetch(
+            "https://jsonplaceholder.typicode.com/comments",
+        ).then((res) => res.json());
 
-    const initData = res.slice(0.20).map((it)=>{
-        return{
-          author:it.email,
-          content:it.body,
-          emotion : Math.floor(Math.random()*5)+1,
-          created_date : new Date().getTime(),
-          id : dataId.current++,
-        }
-    });
-    setData(initData);
-  };
-
-  useEffect(()=>{
-    getData();
-  },[]);
-
-  const onCreate = (author,content,emotion) => {
-    const created_date = new Date().getTime();
-    const newItem = {
-      author,
-      content,
-      emotion,
-      created_date,
-      id: dataId.current,
+        const initData = res.slice(0, 20).map((it) => {
+            return {
+                author: it.email,
+                content: it.body,
+                emotion: Math.floor(Math.random() * 5) + 1,
+                created_date: new Date().getTime(),
+                id: dataId.current++,
+            };
+        });
+        dispatch({ type: "INIT", data: initData });
     };
-    dataId.current += 1;
-    setData([newItem, ...data]);
-  };
 
-  const onRemove = (targetId)=>{
-      console.log("Id가 삭제되었습니다")
-      const newDiarytList = data.filter((it)=> it.id !==targetId);
-      setData(newDiarytList);
-  };
+    useEffect(() => {
+        getData();
+    }, []);
 
-  const onEdit = (targetId, newContent) =>{
-    setData(
-      data.map((it)=> 
-      it.id ===targetId ? {...it, content : newContent} : it
-      )
+    const onCreate = useCallback((author, content, emotion) => {
+        dispatch({
+            type: "CREATE",
+            data: { author, content, emotion, id: dataId.current },
+        });
+        dataId.current += 1;
+    }, []);
+
+    const onRemove = useCallback((targetId) => {
+        dispatch({ type: "REMOVE", targetId });
+    }, []);
+
+    const onEdit = useCallback((targetId, newContent) => {
+        dispatch({ type: "EDIT", targetId, newContent });
+    }, []);
+
+    const getDiaryAnalysis = useMemo(() => {
+        const goodCount = data.filter((it) => it.emotion >= 3).length;
+        const badCount = data.length - goodCount;
+        const goodRatio = (goodCount / data.length) * 100;
+
+        return { goodCount, badCount, goodRatio };
+    }, [data.length]);
+
+    const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
+    return (
+        <div className="App">
+            <OptimizeTest />
+            {/* <Lifecycle/> */}
+            <DiaryEditor onCreate={onCreate} />
+            <div>전체 일기 : {data.length}</div>
+            <div>기분이 좋은 일기 개수 : {goodCount}</div>
+            <div>기분이 나쁜 일기 개수: {badCount}</div>
+            <div>기분 좋은 일기; 비율 : {goodRatio}</div>
+            <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
+        </div>
     );
-  };
-
-  return (
-    
-    <div className="App">
-      {/* <Lifecycle/> */}
-      <DiaryEditor onCreate ={onCreate}/>
-      <DiaryList onEdit ={onEdit} onRemove={onRemove} diaryList={data}/>
-    </div>
-  );
 }
 
 export default App;
--- react/simplediary/src/DiaryEditor.js
+++ react/simplediary/src/DiaryEditor.js
@@ -1,83 +1,81 @@
-import { useRef,useState } from "react";
+import React, { useRef, useState } from "react";
 
-const DiaryEditor = ({onCreate})=>{
+const DiaryEditor = ({ onCreate }) => {
+    const authorInput = useRef();
+    const contentInput = useRef();
 
+    const [state, setstate] = useState({
+        author: "",
+        content: "",
+        emotion: 1,
+    });
 
-  const authorInput = useRef();
-  const contentInput = useRef();
+    const handleChangeState = (e) => {
+        setstate({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-  const [state, setstate] = useState({
-    author:"",
-    content:"",
-    emotion: 1,
-  });
+    const handlesubmmit = () => {
+        if (state.author.length < 1) {
+            //focus
+            authorInput.current.focus();
+            return;
+        }
 
+        if (state.content.length < 5) {
+            //focus
+            contentInput.current.focus();
+            return;
+        }
+        onCreate(state.author, state.content, state.emotion);
+        alert("저장성공");
+        setstate({
+            author: "",
+            content: "",
+            emotion: 1,
+        });
+    };
 
-  const handleChangeState = (e)=>{
-    
-    setstate({
-      ...state,
-      [e.target.name]: e.target.value,
-    })
-  };
+    return (
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+                <input
+                    ref={authorInput}
+                    name="author"
+                    value={state.author}
+                    onChange={handleChangeState}
+                />
+            </div>
+            <div>
+                <textarea
+                    ref={contentInput}
+                    name="content"
+                    value={state.content}
+                    onChange={handleChangeState}
+                />
+            </div>
 
-  const handlesubmmit =() =>{
-    if(state.author.length <1){
-      //focus
-      authorInput.current.focus();
-      return;
-    }
+            <div>
+                <select
+                    name="emotion"
+                    value={state.emotion}
+                    onChange={handleChangeState}
+                >
+                    <option value={1}>1 </option>
+                    <option value={2}>2 </option>
+                    <option value={3}>3 </option>
+                    <option value={4}>4 </option>
+                    <option value={5}>5 </option>
+                </select>
+            </div>
+            <div>
+                <button onClick={handlesubmmit}>일기 저장하기</button>
+            </div>
+        </div>
+    );
+};
 
-    if(state.content.length <5){
-      //focus
-      contentInput.current.focus();
-      return;
-
-    }
-      onCreate(state.author, state.content,state.emotion);
-      alert("저장성공");
-      setstate({
-        author:"",
-        content:"",
-        emotion:1,
-
-      })
-  };
-
-
-
-  return (
-  <div className='DiaryEditor'>
-    <h2>오늘의 일기</h2>
-    <div>
-      <input 
-        ref={authorInput}
-        name ="author"
-        value={state.author}
-        onChange={handleChangeState}/>
-    </div>
-    <div>
-      <textarea
-      ref={contentInput}
-      name = "content"  
-      value={state.content}
-      onChange={handleChangeState}/>
-    </div>
-
-    <div>
-      <select name="emotion" value={state.emotion} onChange={handleChangeState}>
-          <option value={1}>1 </option>
-          <option value={2}>2 </option>
-          <option value={3}>3 </option>
-          <option value={4}>4 </option>
-          <option value={5}>5 </option>
-      </select>
-    </div>
-    <div>
-      <button onClick={handlesubmmit}>일기 저장하기</button>
-    </div>
-  </div>
-  );
-}
-
-export default DiaryEditor;
+export default React.memo(DiaryEditor);
