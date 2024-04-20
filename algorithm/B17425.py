6f2d5e46bfb497ad4a4236b2b8c72b662c542b7d
문지호 <moonwlgh@gmail.com>
2024-04-20 12:49:59 +0900

    백준 17425 solve

--- algorithm/B17425.py
+++ algorithm/B17425.py
@@ -1,10 +1,19 @@
+import sys
+
+g = [0] * 1000001
+f = [0] * 1000001
+
+for i in range(1,1000001):
+    j =1
+    while(i*j <= 1000000):
+        f[i*j] += i
+        j +=1
+    g[i] = g[i-1]+ f[i]
+
+
+
 T = int(input())
 
 for _ in range(T):
-    n = int(input())
-    cnt = 0
-
-    for i in range (1, n+1):
-        cnt += (n//i)*i
-
-    print(cnt)    
+    a = int(sys.stdin.readline())
+    sys.stdout.write(str(g[a]) + "\n")
