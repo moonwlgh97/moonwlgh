bde9480965549c10e03f40c65effe1492630e2aa
moonwlgh <moonwlgh@gmail.com>
2024-04-24 14:17:08 +0900

    백준 6588 solve

--- algorithm/B6588.py
+++ algorithm/B6588.py
@@ -1,44 +1,23 @@
-li = [i for i in range(1, 1000001,2)]
-sosu = []
-for i in li:
-    if i ==1:
-        continue
-    else:
-        for j in range(2, int(max(li)**0.5)+1):
-            if i % j == 0:
-                break
-        else:
-            sosu.append(i)
+import sys
+
+sosu = [True]*1000001
+m= int(1000001 **0.5)
+
+for i in range(2,m+1):
+    if sosu[i]:
+        for j in range(i+i, 1000001, i):
+            sosu[j] = False
 
 
 while(1):
-    n = int(input())
+    n = int(sys.stdin.readline())
 
-    if n ==0 :
+    if n ==0:
         break
-
-    n_li =[]
-    a=0
-    b=0
-
-    for i in sosu:
-
-        if i <= n:
-            n_li.append(i)
-        else:
-            break
-
-    for i in range(0, len(n_li)+1):
-        for j in range(len(n_li), i-1, -1):
-            if n_li[i]+n_li[j] == n:
-                a = n_li[i]
-                b = n_li[j]
-                break
     else:
-        print("Goldbach's conjecture is wrong.")
 
-
-    print(f'{n} = {a} + {b}')
-
-
+        for i in range(2, m+1):
+            if sosu[i] and sosu[n-i]:
+                print(f'{n} = {i} + {n-i}')
+                break
 
